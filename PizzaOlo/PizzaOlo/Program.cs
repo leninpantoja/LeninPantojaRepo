8906193ae7a12f64d0c32fba5fe32d21e75033d2
leninpantoja <56708111+leninpantoja@users.noreply.github.com>
2019-12-19 21:24:18 -0700

    Pizza o|o Exercise
    Topping combination: "pepperoni" rank: 1; number of times ordered: 4616
    Topping combination: "mozzarella cheese"; rank: 2; number of times ordered: 1014
    Topping combination: "four cheese"; rank: 3; number of times ordered: 956
    Topping combination: "bacon"; rank: 4; number of times ordered: 732
    Topping combination: "beef"; rank: 5; number of times ordered: 623
    Topping combination: "sausage"; rank: 6; number of times ordered: 402
    Topping combination: "italian sausage"; rank: 7; number of times ordered: 361
    Topping combination: "chicken"; rank: 8; number of times ordered: 229
    Topping combination: "ham"; rank: 9; number of times ordered: 165
    Topping combination: "mushrooms"; rank: 10; number of times ordered: 159
    Topping combination: "black olives"; rank: 11; number of times ordered: 117
    Topping combination: "pepperoni","four cheese"; rank: 12; number of times ordered: 103
    Topping combination: "alredo sauce"; rank: 13; number of times ordered: 101
    Topping combination: "four cheese","pepperoni"; rank: 14; number of times ordered: 100
    Topping combination: "mozzarella cheese","pepperoni"; rank: 15; number of times ordered: 96
    Topping combination: "cheddar cheese"; rank: 16; number of times ordered: 95
    Topping combination: "pineapple"; rank: 17; number of times ordered: 79
    Topping combination: "pepperoni","beef"; rank: 18; number of times ordered: 67
    Topping combination: "pepperoni","bacon"; rank: 19; number of times ordered: 63
    Topping combination: "pepperoni","mozzarella cheese"; rank: 20; number of times ordered: 59

---
+++ PizzaOlo/PizzaOlo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace PizzaOlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myPath = @"E:\Projects\TestProject\PizzaOlo\PizzaOlo\pizzas.json";
+            string myJsonString = ReadJson(myPath); // get jason in a string to separate
+
+            Dictionary<object, int> myToppings = GetToppings(myJsonString); // save toppings in a dictionary with the count
+
+            const int cTop = 20;
+            var myIESortToppings = (from entry in myToppings orderby entry.Value descending select entry).Take(3);
+            List<KeyValuePair<object, int>> mySortToppings = (from entry in myToppings orderby entry.Value descending select entry).Take(cTop).ToList();
+
+            int myRank = 0;
+            foreach (var myTopping in mySortToppings)
+            {
+                myRank++;
+                //string myToppingString = myTopping.Key.ToString().Split
+                Console.WriteLine("Topping combination: {0}; rank: {1}; number of times ordered: {2}", myTopping.Key, myRank, myTopping.Value);
+            }
+
+            Console.ReadKey();
+        }
+        static string ReadJson(string pPath)
+        {
+            string myJsonString;
+
+            using (StreamReader r = new StreamReader(pPath))
+            {
+                myJsonString = r.ReadToEnd();
+            }
+
+            return myJsonString;
+        }
+
+        static Dictionary<object, int> GetToppings(string pJsonString)
+        {
+            Dictionary<object, int> myToppings = new Dictionary<object, int>();
+
+            var objects = JArray.Parse(pJsonString);
+            foreach (var toppings in objects)
+            {
+                string myTopping = toppings.ToString();
+                if (myToppings.ContainsKey(myTopping))
+                {
+                    myToppings[myTopping] += 1;
+                }
+                else
+                {
+                    myToppings.Add(myTopping, 1);
+                }
+            }
+
+            return myToppings;
+        }
+
+        static Dictionary<object, int> GetToppings2(string pJsonString)
+        {
+            Dictionary<object, int> myToppings = new Dictionary<object, int>();
+
+            var objects = JArray.Parse(pJsonString);
+            foreach (var toppings in objects)
+            {
+                string myTopping = toppings.ToString();
+                JToken myTest = toppings["toppings"].Value<JToken>();
+                string myVal = myTest.ToString();
+                if (myToppings.ContainsKey(myTopping))
+                {
+                    myToppings[myTopping] += 1;
+                }
+                else
+                {
+                    myToppings.Add(myTopping, 1);
+                }
+            }
+
+            return myToppings;
+        }
+    }
+}
