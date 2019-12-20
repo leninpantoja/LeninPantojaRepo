d0fe1e92b284bd8761afb65b8ddf1475d1204d61
Lenin Pantoja <Lenin.Pantoja@thomsonreuters.com>
2019-12-20 11:20:16 -0700

    Refactored code into a new class.

--- PizzaOlo/PizzaOlo/Program.cs
+++ PizzaOlo/PizzaOlo/Program.cs
@@ -8,40 +8,45 @@ namespace PizzaOlo
 {
     class Program
     {
+        private const string cJsonFileName = @"..\..\pizzas.json";
+        private const int cTopMostPopularToppings = 20;
+
         static void Main(string[] args)
         {
-            // get json in a string to separate
-            const string myPath = @"..\..\pizzas.json";
-            string myJsonString = ReadJson(myPath);
-            // get List of toppings
-            List<string> myListOfToppings = GetListOfToppings(myJsonString);
-            // get Dictionary with the Key combination of topping and the number of order
-            Dictionary<string, int> myDictionaryOfToppings = GetDictionaryOfToppings(myListOfToppings);
-            // order by Value desc ONLY the 20 rows
-            const int cTop = 20;
-            List<KeyValuePair<string, int>> mySortToppings = (from entry in myDictionaryOfToppings orderby entry.Value descending select entry).Take(cTop).ToList();
-            // print Results
-            int myRank = 1;
-            foreach (var myTopping in mySortToppings)
-            {
-                Console.WriteLine("Topping combination: {0}; Rank: {1}; Number of times ordered: {2}", myTopping.Key, myRank++, myTopping.Value);
-            }
+            // create my PizzaToppings object
+            PizzaToppings myPizzaToppings = new PizzaToppings(cJsonFileName);
+            // print results with the Top Most Popular Toppings
+            myPizzaToppings.PrintResults(cTopMostPopularToppings);
             // wait
             Console.ReadKey();
         }
-        static string ReadJson(string pPath)
+    }
+
+    public class PizzaToppings
+    {
+        private Dictionary<string, int> _DictionaryOfToppings;
+
+        public PizzaToppings(string pJsonFileName)
+        {
+            // get json in a string to separate
+            string myJsonString = ReadJson(pJsonFileName);
+            // get List of toppings
+            List<string> myListOfToppings = GetListOfToppings(myJsonString);
+            // get Dictionary with the Key combination of topping and the number of order
+            _DictionaryOfToppings = GetDictionaryOfToppings(myListOfToppings);
+        }
+        private string ReadJson(string pJsonFileName)
         {
             string myJsonString;
 
-            using (StreamReader r = new StreamReader(pPath))
+            using (StreamReader r = new StreamReader(pJsonFileName))
             {
                 myJsonString = r.ReadToEnd();
             }
 
             return myJsonString;
         }
-
-        static List<string> GetListOfToppings(string pJsonString)
+        private List<string> GetListOfToppings(string pJsonString)
         {
             List<string> myListOfToppings = new List<string>();
 
@@ -55,11 +60,11 @@ namespace PizzaOlo
 
             return myListOfToppings;
         }
-        static Dictionary<string, int> GetDictionaryOfToppings(List<string> myListOfToppings)
+        private Dictionary<string, int> GetDictionaryOfToppings(List<string> pListOfToppings)
         {
             Dictionary<string, int> myDictionaryOfToppings = new Dictionary<string, int>();
 
-            foreach (string myTopping in myListOfToppings)
+            foreach (string myTopping in pListOfToppings)
             {
                 if (myDictionaryOfToppings.ContainsKey(myTopping))
                 {
@@ -72,6 +77,17 @@ namespace PizzaOlo
             }
 
             return myDictionaryOfToppings;
+        }
+        public void PrintResults(int pTopMostPopularToppings)
+        {
+            // order by Value desc ONLY the top rows required
+            List<KeyValuePair<string, int>> mySortToppings = (from entry in _DictionaryOfToppings orderby entry.Value descending select entry).Take(pTopMostPopularToppings).ToList();
+            // print Results
+            int myRank = 1;
+            foreach (KeyValuePair<string, int> myTopping in mySortToppings)
+            {
+                Console.WriteLine("Topping combination: {0}; Rank: {1}; Number of times ordered: {2}", myTopping.Key, myRank++, myTopping.Value);
+            }
         }
     }
 }
